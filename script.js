62193d33802fb1e8c6cb9dca34d21a78883635b1
Trungenial <transytrung0752.work@gmail.com>
2025-04-03 22:55:52 +0700

    Create checkWin() function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const game = (() => {
-    function createPlayer(name, marker, coordinate) {
-        return { name, marker, coordinate };
+    function createPlayer(name, marker, indexValue) {
+        return { name, marker, indexValue };
     }
 
     function createBoard(size) {
@@ -16,10 +16,58 @@ const game = (() => {
 
     function addMark(player, board) {
         //Coordinate of the current marker of the player
-        const x = player.coordinate.x;
-        const y = player.coordinate.y;
+        const i = player.indexValue.i;
+        const j = player.indexValue.j;
 
-        board[x][y] = player.marker;
+        board[i][j] = player.marker;
     }
-    return { createBoard, createPlayer, addMark };
+
+    function checkWin(board, player, n) {
+        const rows = board.length;
+        const cols = board[0].length;
+
+        function countDirection(iStep, jStep) {
+            let count = 1;
+            let i = player.indexValue.i + iStep,
+                j = player.indexValue.j + jStep;
+
+            // Go forward
+            while (
+                i >= 0 &&
+                j >= 0 &&
+                i < rows &&
+                j < cols &&
+                board[i][j] === player.marker
+            ) {
+                count++;
+                i += iStep;
+                j += jStep;
+            }
+
+            // Go backward
+            i = player.indexValue.i - iStep;
+            j = player.indexValue.j - jStep;
+            while (
+                i >= 0 &&
+                j >= 0 &&
+                i < rows &&
+                j < cols &&
+                board[i][j] === player.marker
+            ) {
+                count++;
+                i -= iStep;
+                j -= jStep;
+            }
+
+            return count >= n;
+        }
+
+        return (
+            countDirection(0, 1) || // Horizontal
+            countDirection(1, 0) || // Vertical
+            countDirection(1, 1) || // Main diagonal
+            countDirection(-1, -1) // Anti- diagonal
+        );
+    }
+    return { createBoard, createPlayer, addMark, checkWin };
 })();
